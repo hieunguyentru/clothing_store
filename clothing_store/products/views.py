2a9a70eb82cad7045f94b8aa0187c6aba584f186
hieunguyentru <hieucho1207@gmail.com>
2025-09-10 21:24:52 +0700

    Lần thứ 1

--- clothing_store/products/views.py
+++ clothing_store/products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+def index(request):
+    return render(request, 'products/index.html')
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product.html', {'product': product})
+
